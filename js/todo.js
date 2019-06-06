4ccf6c5d92f0282335768fe041fec75e27ade578
Nadya Ilinskaya <nadya.ilinskiy@gmail.com>
2019-06-06 00:11:56 -0700

    adding saving to localStorage and Load from LocalStorage function

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,7 @@
 /////////////// should waiting for DOM loading be part of every script?
 document.addEventListener("DOMContentLoaded", function() {
     addTodo();
+    loadTodo();
   });
 
 // on click Add button -  add new todo (text from input to the todo list)
@@ -16,16 +17,29 @@ document.addEventListener("DOMContentLoaded", function() {
         let newLi = document.createElement("Li");
         let userInput = document.getElementById("usrInput").value;
         let newTask = document.createTextNode(userInput);
-     
+        
         console.log(newTask);
         newLi.appendChild(newTask);
         document.getElementById("todo").appendChild(newLi);
+        let myList = document.querySelector("ul");
+        localStorage.setItem ("myToDoList", myList.innerHTML);
+        
     });
    document.getElementById("usrInput").value = "";
+   
+
  }
+
+ function loadTodo(){
+    if(localStorage.getItem("myToDoList")){
+        let myList = document.querySelector("ul");
+        myList.innerHTML = localStorage.getItem("myToDoList");
+    }
+}
 
 
 //on click Delete "X" button remove item from the todo list
 function deleteTodo(){
    
 }
+
